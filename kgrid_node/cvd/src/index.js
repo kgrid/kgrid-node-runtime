82271cfd58b8c4fbf672714e47f9a849d7cbeeac
mradamgbeck <mradamgbeck@gmail.com>
2021-10-25 16:10:05 -0400

    deleted shelf

--- kgrid_node/cvd/src/index.js
+++
@@ -1,9 +0,0 @@
-const qalyoutcome = require('@kgrid/qalyutil/qalyoutcome')
-const config = require('./config.json')
-
-function qalygain(inputs){
-  let output = qalyoutcome(__dirname, config, inputs)
-  return output
-}
-
-module.exports= qalygain
